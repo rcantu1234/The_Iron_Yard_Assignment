260e03e684ae4b5d5d48191ad0f47df10e6a64c3
ROEL CANTU <roel.cantu@yahoo.com>
2015-01-21 19:35:18 -0600

    Editing

--- db/migrate/20150121210547_add_fields_values_to_ironyards.rb
+++ db/migrate/20150121210547_add_fields_values_to_ironyards.rb
@@ -4,5 +4,7 @@ class AddFieldsValuesToIronyards < ActiveRecord::Migration
     add_column :ironyards, :lastname, :string
     add_column :ironyards, :age, :string
     add_column :ironyards, :location, :string
+    add_column :campuses, :campusname, :string
+    add_column :campuses, :campuslocation, :string
   end
 end
