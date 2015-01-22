a5251d5f89e823c79ac003ecee3806650a232685
ROEL CANTU <roel.cantu@yahoo.com>
2015-01-21 20:06:16 -0600

    Editing

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,6 @@ class WelcomeController < ApplicationController
   def index
     @ironyards = ironyard.all
     @campuses = campse.all
-
+    @students = student.all
   end
 end
--- db/migrate/20150122015637_create_students.rb
+++ db/migrate/20150122015637_create_students.rb
@@ -1,6 +1,10 @@
 class CreateStudents < ActiveRecord::Migration
   def change
     create_table :students do |t|
+      t.string :firstname, null: false, limit: 40
+      t.string :lastname, null: false, limit: 50
+      t.string :address, null: false, limit: 60
+
 
       t.timestamps null: false
     end
