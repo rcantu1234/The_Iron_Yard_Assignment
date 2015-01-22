a32b1d3e2a0cbeade93486f79bf8f0aa6996047c
ROEL CANTU <roel.cantu@yahoo.com>
2015-01-21 19:57:10 -0600

    Editing

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150122005340) do
+ActiveRecord::Schema.define(version: 20150122015637) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,11 @@ ActiveRecord::Schema.define(version: 20150122005340) do
     t.string   "lastname"
     t.string   "age"
     t.string   "location"
+  end
+
+  create_table "students", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
