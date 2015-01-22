6f2edbf4e086309016db84d52675bc0dc097caa4
ROEL CANTU <roel.cantu@yahoo.com>
2015-01-21 18:13:48 -0600

    Edit

--- db/migrate/20150121202426_create_ironyards.rb
+++ db/migrate/20150121202426_create_ironyards.rb
@@ -1,6 +1,8 @@
 class CreateIronyards < ActiveRecord::Migration
   def change
     create_table :ironyards do |t|
+      t.string :campusname, null: false, limit: 40
+      t.string :campuslocation, null: false, limit: 40
 
       t.timestamps null: false
     end
