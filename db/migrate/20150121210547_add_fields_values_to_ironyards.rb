88cafc6c6f62214aebfdce4a976ae00c2d66fda8
ROEL CANTU <roel.cantu@yahoo.com>
2015-01-22 09:13:18 -0600

    adding factor

--- db/migrate/20150121210547_add_fields_values_to_ironyards.rb
+++
@@ -1,8 +0,0 @@
-class AddFieldsValuesToIronyards < ActiveRecord::Migration
-  def change
-    add_column :ironyards, :firstname, :string
-    add_column :ironyards, :lastname, :string
-    add_column :ironyards, :age, :string
-    add_column :ironyards, :location, :string
-  end
-end
--- db/migrate/20150122025130_add_fields_values_to_ironyards.rb
+++
@@ -1,6 +0,0 @@
-class AddFieldsValuesToIronyards < ActiveRecord::Migration
-  def change
-    add_column :ironyards, :instructorsname, :string
-    add_column :ironyards, :course, :string
-  end
-end
